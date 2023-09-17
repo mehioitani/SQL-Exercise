668b8166dc808d230bbe6dfd24b48c88ddb127f1
mehioitani <muhieddineitani04@gmail.com>
2023-09-17 22:35:52 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,25 @@
+Problem 1: SELECT * FROM students;
+
+
+Problem 2: SELECT name FROM students;
+
+Problem 3: SELECT * FROM students
+WHERE Age > "30";
+
+Problem 4: SELECT * FROM students
+WHERE Gender = "f"
+AND Age = "30";
+
+Problem 5: SELECT Points FROM students
+WHERE name = "Alex"
+
+Problem 6: INSERT INTO students(ID,Name,Age,Gender,Points)
+VALUES('7','Muhieddine','29','M','700');
+
+Problem 7: UPDATE students
+SET Points = "400"
+WHERE Name = "Basma";
+
+Problem 8: UPDATE students
+SET Points = "100"
+WHERE name = "Alex";
