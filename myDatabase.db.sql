b3ec6d609f54d3b99bdd3d1abfbe353eac90ac0d
mehioitani <muhieddineitani04@gmail.com>
2023-09-18 11:35:19 +0300

    Backup Done

---
+++ myDatabase.db.sql
@@ -0,0 +1,27 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "students" (
+	"ID"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	TEXT,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	PRIMARY KEY("ID" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "graduates" (
+	"ID"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	INTEGER,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	"Graduation"	TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT)
+);
+INSERT INTO "students" VALUES (1,'Alex','21','M',100);
+INSERT INTO "students" VALUES (2,'Basma','25','F',400);
+INSERT INTO "students" VALUES (3,'Hasan','30','M',150);
+INSERT INTO "students" VALUES (4,'Layal',NULL,NULL,NULL);
+INSERT INTO "students" VALUES (5,'Robert','34','M',500);
+INSERT INTO "students" VALUES (6,'Jana','33','F',500);
+INSERT INTO "students" VALUES (7,'Muhieddine','29','M',700);
+INSERT INTO "graduates" VALUES (2,'Layal',18,'F',350,'08/09/2018');
+COMMIT;
